ae8e9718e9ca99466e3fb617e35436a6044b9d55
Scott Inkley <scotti@qualtrics.com>
2023-12-06 15:35:04 -0700

    Adds template file and dec05 files

---
+++ dec05.test.js
@@ -0,0 +1,12 @@
+const {} = require('./dec05')
+
+const EXAMPLE =
+``
+
+const FULL_INPUT =
+``
+
+test('solutions', () => {
+    console.log('Part 1 answer: ')
+    console.log('Part 2 answer: ')
+})
