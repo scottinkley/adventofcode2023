8b9a43bb678683bb5dd07596b595ffb79cd0b3e8
Scott Inkley <scotti@qualtrics.com>
2023-12-07 13:39:18 -0700

    Dec 5 part 1 refactor 1

--- dec05.js
+++ dec05.js
@@ -5,7 +5,8 @@ If You Give A Seed A Fertilizer
 function getLowestLocationOfSeeds(input) {
     const rows = input.trim().split('\n')
     const seeds = getNumbers(rows[0].trim().split(':')[1])
-    const locations = seeds.map(seed => getLocationOfSeed(seed, rows))
+    // const locations = seeds.map(seed => getLocationOfSeed(seed, rows))
+    const locations = getLocationsOfSeeds(seeds, rows)
     return locations.reduce((a, b) => Math.min(a, b), Number.MAX_SAFE_INTEGER)
 }
 
@@ -27,6 +28,40 @@ function getLocationOfSeed(seed, rows) {
         index++
     }
     return number
+}
+
+function getLocationsOfSeeds(seeds, rows) {
+    function mapNumbers(current) {
+        const result = new Map()
+        while (index < rows.length && rows[index].length > 1) {
+            const row = getNumbers(rows[index])
+            for (let [key, number] of current) {
+                if (number >= row[1] && number <= row[1] + row[2] - 1) {
+                    result.set(key, number += row[0] - row[1])
+                    current.delete(key)
+                }
+            }
+            index++
+        }
+        for (let [key, number] of current) {
+            result.set(key, number)
+        }
+        return result
+    }
+
+    let currentNums = new Map()
+    for (let i = 0; i < seeds.length; i++) {
+        currentNums.set(i, seeds[i])
+    }
+    let index = 1
+    while (index < rows.length) {
+        if (rows[index].includes('map')) {
+            index++
+            currentNums = mapNumbers(currentNums)
+        }
+        index++
+    }
+    return Array.from(currentNums.values())
 }
 
 function getNumbers(string) {
