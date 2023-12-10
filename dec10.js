9659c92fe677a7d31cdfe6a4ffeef4764b605a54
Scott Inkley <scotti@qualtrics.com>
2023-12-10 12:39:20 -0700

    Adds Dec 10 files

---
+++ dec10.js
@@ -0,0 +1,13 @@
+/*
+Day 10: Pipe Maze
+*/
+
+/*
+Idea -- use a breadth-first search, starting at S
+
+if a location in the queue already has a distance, replace that distance if is larger than the current calculated distance
+
+May want to create a locations 2d array
+*/
+
+module.exports = {}
