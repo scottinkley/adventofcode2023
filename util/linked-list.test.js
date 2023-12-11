f3ecfe740576f75ade9bfced6967f9528a0d64af
Scott Inkley <scottinkley@gmail.com>
2023-12-11 01:28:50 -0700

    Refactors LinkedList tests

--- util/linked-list.test.js
+++ util/linked-list.test.js
@@ -2,86 +2,29 @@ const { LinkedList } = require('./linked-list')
 const { NoSuchElementError } = require('./errors')
 
 describe('LinkedList', () => {
-    describe('addFirst()', () => {
-        test('add to empty list', () => {
-            const list = new LinkedList()
-            list.addFirst(5)
-            expect(list.first).toEqual(5)
-            expect(list.last).toEqual(5)
+    describe('list is empty', () => {
+        let list
+        beforeAll(() => {
+            list = new LinkedList()
         })
 
-        test('add to list with one node', () => {
-            const list = new LinkedList()
-            list.addFirst(5)
-            list.addFirst(3)
-            expect(list.first).toEqual(3)
-            expect(list.last).toEqual(5)
-        })
-    })
-
-    describe('addLast()', () => {
-        test('add to empty list', () => {
-            const list = new LinkedList()
-            list.addLast(7)
-            expect(list.first).toEqual(7)
-            expect(list.last).toEqual(7)
-        })
-
-        test('add to list with one node', () => {
-            const list = new LinkedList()
-            list.addLast(7)
-            list.addLast(4)
-            expect(list.last).toEqual(4)
-            expect(list.first).toEqual(7)
-        })
-    })
-
-    describe('first()', () => {
-        test('empty list', () => {
-            const list = new LinkedList()
-            expect(() => list.first).toThrow(NoSuchElementError)
-        })
-    })
-
-    describe('last()', () => {
-        test('empty list', () => {
-            const list = new LinkedList()
-            expect(() => list.last).toThrow(NoSuchElementError)
-        })
-    })
-
-    describe('removeFirst()', () => {
-        test('empty list', () => {
-            const list = new LinkedList()
-            expect(() => list.removeFirst()).toThrow(NoSuchElementError)
-        })
-
-        test('list has one node', () => {
-            const list = new LinkedList()
-            list.addFirst(8)
-            const removedValue = list.removeFirst()
-            expect(removedValue).toEqual(8)
-            expect(() => list.first).toThrow(NoSuchElementError)
-        })
-
-        test('list has more than one node', () => {
-            const list = new LinkedList()
-            list.addFirst(9)
-            list.addFirst(2)
-            const removedValue = list.removeFirst()
-            expect(removedValue).toEqual(2)
-            expect(list.first).toEqual(9)
-        })
-    })
-
-    describe('size()', () => {
-        test('emtpy list', () => {
-            const list = new LinkedList()
+        it('should have a size of 0', () => {
             expect(list.size).toEqual(0)
         })
 
-        it('should not go negative when removing from an empty list', () => {
-            const list = new LinkedList()
+        it('should throw an error when trying to get first element', () => {
+            expect(() => list.first).toThrow(NoSuchElementError)
+        })
+
+        it('should throw an error when trying to get last element', () => {
+            expect(() => list.last).toThrow(NoSuchElementError)
+        })
+
+        it('should throw an error when trying to remove first element', () => {
+            expect(() => list.removeFirst()).toThrow(NoSuchElementError)
+        })
+
+        it('should not have a negative size when trying to remove first element', () => {
             try {
                 list.removeFirst()
             } catch(error) {
@@ -89,37 +32,144 @@ describe('LinkedList', () => {
             }
             expect(list.size).toEqual(0)
         })
+    })
 
-        describe('list with one node', () => {
-            let list
+    describe('one element is added first', () => {
+        let list
+        const addedValue = 8
+        beforeEach(() => {
+            list = new LinkedList()
+            list.addFirst(addedValue)
+        })
+
+        it('should return that element as the first element', () => {
+            expect(list.first).toEqual(addedValue)
+        })
+
+        it('should return that element as the last element', () => {
+            expect(list.last).toEqual(addedValue)
+        })
+
+        it('should have a size of 1', () => {
+            expect(list.size).toEqual(1)
+        })
+    })
+
+    describe('one element is added last', () => {
+        let list
+        const addedValue = 2
+        beforeEach(() => {
+            list = new LinkedList()
+            list.addLast(addedValue)
+        })
+
+        it('should return that element as the first element', () => {
+            expect(list.first).toEqual(addedValue)
+        })
+
+        it('should return that element as the last element', () => {
+            expect(list.last).toEqual(addedValue)
+        })
+
+        it('should have a size of 1', () => {
+            expect(list.size).toEqual(1)
+        })
+    })
+
+    describe('list has one element', () => {
+        let list
+        const originalValue = 5
+        beforeEach(() => {
+            list = new LinkedList()
+            list.addFirst(originalValue)
+        })
+
+        describe('element is added first', () => {
+            const newValueAdded = 23
             beforeEach(() => {
-                list = new LinkedList()
-                list.addFirst(3)
+                list.addFirst(newValueAdded)
             })
 
-            it('should have a size of 1', () => {
-                expect(list.size).toEqual(1)
+            it('should return the new value as first', () => {
+                expect(list.first).toEqual(newValueAdded)
             })
-    
-            test('list with many nodes', () => {
-                list.addFirst(9)
-                list.addFirst(-1)
-                expect(list.size).toEqual(3)
+
+            it('should return the original value as last', () => {
+                expect(list.last).toEqual(originalValue)
             })
-    
-            test('addLast', () => {
-                list.addLast(11)
-                expect(list.size).toEqual(2)
+        })
+
+        describe('element is added last', () => {
+            const newValueAdded = 16
+            beforeEach(() => {
+                list.addLast(newValueAdded)
             })
-    
-            test('removeFirst', () => {
-                list.removeFirst()
+
+            it('should return the new value as last', () => {
+                expect(list.last).toEqual(newValueAdded)
+            })
+
+            it('should return the original value as first', () => {
+                expect(list.first).toEqual(originalValue)
+            })
+        })
+
+        describe('element is removed', () => {
+            let removedValue
+            beforeEach(() => {
+                removedValue = list.removeFirst()
+            })
+
+            it('should return the removed value', () => {
+                expect(removedValue).toEqual(originalValue)
+            })
+
+            it('should have a size of 0', () => {
                 expect(list.size).toEqual(0)
+            })
+
+            it('should throw an error when trying to get first element', () => {
+                expect(() => list.first).toThrow(NoSuchElementError)
+            })
+    
+            it('should throw an error when trying to get last element', () => {
+                expect(() => list.last).toThrow(NoSuchElementError)
             })
         })
     })
 
-    test('full workflow', () => {
+    describe('list has three elements', () => {
+        let list
+        const firstValue = 3
+        const secondValue = -4
+        const thirdValue = 18
+        beforeEach(() => {
+            list = new LinkedList()
+            list.addFirst(thirdValue)
+            list.addFirst(secondValue)
+            list.addFirst(firstValue)
+        })
+
+        it('should have a size of 3', () => {
+            expect(list.size).toEqual(3)
+        })
+
+        describe('first value is removed', () => {
+            beforeEach(() => {
+                list.removeFirst()
+            })
+
+            it('should return second value as first', () => {
+                expect(list.first).toEqual(secondValue)
+            })
+
+            it('should return third value as last', () => {
+                expect(list.last).toEqual(thirdValue)
+            })
+        })
+    })
+
+    test('workflow', () => {
         const list = new LinkedList()
         expect(() => list.first).toThrow(NoSuchElementError)
         expect(() => list.last).toThrow(NoSuchElementError)
